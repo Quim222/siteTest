3edc6186c07873ea5cc0cd3f0af142a1bc6ca58c
Quim222 <joaquim.a.f.sima04@gmail.com>
2024-08-31 20:14:04 +0100

    Variaveis guardadas

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAyHHLVkO9_eKxEPILNnWBEBl-a7SXWOBw",
-    authDomain: "petalert-755d9.firebaseapp.com",
-    projectId: "petalert-755d9",
-    storageBucket: "petalert-755d9.appspot.com",
-    messagingSenderId: "336183360505",
-    appId: "1:336183360505:web:948672b6529b5377eb5b28"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 // Inicializando Firebase
